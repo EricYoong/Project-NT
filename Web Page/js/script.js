a9e7661b0282b8df5d0a61038f5716994a164cd7
Eric <devilking9820@hotmail.com>
2018-10-16 11:01:49 +0800

    update some text and photo

--- Web Page/js/script.js
+++ Web Page/js/script.js
@@ -1,6 +1,20 @@
 (function ($) {
     'use strict';
 
+    $('.item2 li').hover(function () {
+        $(this).find('svg.fa-arrow-circle-right').removeClass("hidden1");
+        $(this).find('svg.fa-circle').addClass("hidden1");
+    }, function () {
+        $(this).find('svg.fa-arrow-circle-right').addClass("hidden1");
+        $(this).find('svg.fa-circle').removeClass("hidden1");
+    });
+
+    $('.image-content').hover(function () {
+        $(this).find('.circle-icon').toggleClass("circle-icon-hover");
+    }, function () {
+        $(this).find('.circle-icon').toggleClass("circle-icon-hover");
+    });
+
     $('a[href="#"]').click(function ($) {
         $.preventDefault()
     });
